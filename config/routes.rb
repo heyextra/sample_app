56a667df708ef0abbfb1e29ace5afa6e7e4d15ae
heyextra <extrafahrali@gmail.com>
2022-07-18 00:17:51 +0000

    add hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "application#hello"
   # SYNTAX : root "controller_name#action_name"
-  root "users#index"
+
 
 end
