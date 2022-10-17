da6140924c09a7a5f9f2ed16dfd94cc96715f4e6
heyextra <extrafahrali@gmail.com>
2022-10-17 16:28:00 -0400

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
 root "static_pages#home"
+get 'help'    => 'static_pages#help'
+get 'about'   => 'static_pages#about'
+get 'contact' => 'static_pages#contact'
+get 'signup' => 'users#new'
 # Defines the root path route ("/")
 # SYNTAX : root "controller_name#action_name"
-
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,30 +6,27 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @basetitle = "Ruby on Rails Tutorial Sample App"
   end
    test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
+    assert_select "title", "Home | #{@basetitle}"
   end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "#{@basetitle}"
-  end
+
 # Selector, content
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@basetitle}"
   end
 
    test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@basetitle}"
   end
 
     test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@basetitle}"
   end
